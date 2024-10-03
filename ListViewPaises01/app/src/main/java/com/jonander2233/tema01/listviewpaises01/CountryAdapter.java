a8d85ed6b36f0cbe3cabb1137af7ba807d5812eb
Jon Ander <jonander2233@gmail.com>
2024-10-03 13:36:50 +0200

    wip

---
+++ ListViewPaises01/app/src/main/java/com/jonander2233/tema01/listviewpaises01/CountryAdapter.java
@@ -0,0 +1,22 @@
+package com.jonander2233.tema01.listviewpaises01;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CountryAdapter extends ArrayAdapter<Country> {
+    public CountryAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+        
+    }
+}
--- ListViewPaises01/app/src/main/java/com/jonander2233/tema01/listviewpaises01/MainActivity.java
+++ ListViewPaises01/app/src/main/java/com/jonander2233/tema01/listviewpaises01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jonander2233.tema01.listviewpaises01;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listaPaises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
