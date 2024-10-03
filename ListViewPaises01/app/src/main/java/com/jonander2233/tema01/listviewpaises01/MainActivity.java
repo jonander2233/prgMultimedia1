693b961b4dc874f3309781eb505171a10393205f
Jon Ander <jonander2233@gmail.com>
2024-10-03 14:42:38 +0200

    wip

--- ListViewPaises01/app/src/main/java/com/jonander2233/tema01/listviewpaises01/MainActivity.java
+++ ListViewPaises01/app/src/main/java/com/jonander2233/tema01/listviewpaises01/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jonander2233.tema01.listviewpaises01;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.countries,android.R.layout.simple_list_item_1);
+        listaPaises = (ListView)findViewById(R.id.listaPaises);
 
 
     }
