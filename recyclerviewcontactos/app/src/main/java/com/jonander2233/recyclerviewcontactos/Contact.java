9cf4db3908ea286f19c463bb6161d3a41278c9b7
jon ander <jonander2233@gmail.com>
2024-10-10 16:56:06 +0200

    wip

--- recyclerviewcontactos/app/src/main/java/com/jonander2233/recyclerviewcontactos/Contact.java
+++ recyclerviewcontactos/app/src/main/java/com/jonander2233/recyclerviewcontactos/Contact.java
@@ -4,6 +4,18 @@ public class Contact {
     private String name;
     private String firstSurname;
     private String secondSurname;
+    private String phone1;
+    private String phone2;
+    private String email;
+
+    public Contact(String name, String firstSurname, String secondSurname, String phone1, String phone2, String email) {
+        this.name = name;
+        this.firstSurname = firstSurname;
+        this.secondSurname = secondSurname;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
@@ -27,5 +39,29 @@ public class Contact {
 
     public void setSecondSurname(String secondSurname) {
         this.secondSurname = secondSurname;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- recyclerviewcontactos/app/src/main/java/com/jonander2233/recyclerviewcontactos/ContactParser.java
+++ recyclerviewcontactos/app/src/main/java/com/jonander2233/recyclerviewcontactos/ContactParser.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.IOException;
@@ -24,9 +25,20 @@ public class ContactParser {
             in.read(buffer);
             in.close();
             String content = new String(buffer, StandardCharsets.UTF_8);
-//            result = new ArrayList<>();
             JSONTokener jsonTokener = new JSONTokener(content);
             JSONArray array = new JSONArray(jsonTokener);
+            result = new ArrayList<>(array.length());
+            for(int i = 0; i < array.length(); i++) {
+                JSONObject  jsonObject = array.getJSONObject(i);
+                String name = jsonObject.getString("name");
+                String firstname = jsonObject.getString("firstName");
+                String secondSurname = jsonObject.getString("secondName");
+                String phone1 = jsonObject.getString("phone1");
+                String phone2 = jsonObject.getString("phone2");
+                String email = jsonObject.getString("email");
+                result.add(new Contact(name,firstname,secondSurname,phone1,phone2,email));
+
+            }
 
         }catch (IOException ioe){
             Log.e(ContactParser.class.getName(),ioe.getMessage());
