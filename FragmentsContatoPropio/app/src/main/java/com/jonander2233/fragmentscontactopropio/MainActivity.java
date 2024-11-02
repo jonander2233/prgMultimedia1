f2a59e532b0ffdb61d715bf49ad607830775a5e3
Jon Ander <jonander2233@gmail.com>
2024-11-02 18:35:01 +0100

    wip

--- FragmentsContatoPropio/app/src/main/java/com/jonander2233/fragmentscontactopropio/MainActivity.java
+++ FragmentsContatoPropio/app/src/main/java/com/jonander2233/fragmentscontactopropio/MainActivity.java
@@ -1,13 +1,9 @@
-package com.jonander2233.listviewcontactosconrecyclerviewpropio;
+package com.jonander2233.fragmentscontactopropio;
 
 import android.os.Bundle;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
