3455a8245bdf608d750491164bbc923208021d4e
Jon Ander <jonander2233@gmail.com>
2024-11-21 03:02:31 +0100

    wip

--- EjNavigationCorreoTutoriail/app/src/main/java/com/jonander2233/ejnavigationcorreotutoriail/MainActivity.java
+++ EjNavigationCorreoTutoriail/app/src/main/java/com/jonander2233/ejnavigationcorreotutoriail/MainActivity.java
@@ -1,19 +1,50 @@
 package com.jonander2233.ejnavigationcorreotutoriail;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.DragAndDropPermissionsCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.jonander2233.ejnavigationcorreotutoriail.ui.fragments.ListFragment;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ListFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_sent);
+        }
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_sent:
+                break;
+
+        }
+        return false;
     }
 }
